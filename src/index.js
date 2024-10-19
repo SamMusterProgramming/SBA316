f0750155247e32a09825b6ec6a6cf0a94cd514a6
samcoeur <samcoeur2013@gmail.com>
2024-10-18 23:33:20 -0400

    second commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+
+//set up the icons on the sidebar
+const iconContainer = document.getElementById('icon')
+const icons = icon.querySelectorAll('div')
+Array.from(icons, (icon) => {
+    icon.style.fontSize ="18px"
+    icon.style.marginLeft = "25px"
+})
+
